b2d9a5dccbf5b08c4335b41de21b7faaf1fdb586
Desblooms <desblooms@gmail.com>
2025-05-15 18:12:17 +0530

    PWA

---
+++ .history/assets/js/themecolor_20250515181005.js
@@ -0,0 +1,18 @@
+  tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        primary: '#ff5722',
+                        accent: '#ffc107',
+                        background: '#f8f8f8',
+                        dark: '#1a1a1a'
+                    },
+                    fontFamily: {
+                        sans: ['"Poppins"', 'sans-serif']
+                    },
+                    borderRadius: {
+                        xl: '1.2rem'
+                    }
+                }
+            }
+        }
